f82cef2db4faab893e8c722343fd48b684ccdb00
toledothadius <143941608+toledothadius@users.noreply.github.com>
2023-09-17 21:20:02 +0800

    Add files via upload

---
+++ act 4 assignment.py
@@ -0,0 +1,18 @@
+print("Welocme")
+name = input("Name: ")
+print("Hello",name)
+
+it = int(input("IT: "))
+acct = int(input("ACCT: "))
+hr = int(input("HR: "))
+
+payit = 1000
+payacct = 1200
+payhr = 1500
+
+x = it * payit
+a = acct  * payacct
+h = hr * payhr
+print(name,"you earn in IT for the yeas you've work is: ",x)
+print(name,"you earn in ACCT is for the yeas you've work is: ",a)
+print(name,"you earn in HR is for the yeas you've work is: ",h)
